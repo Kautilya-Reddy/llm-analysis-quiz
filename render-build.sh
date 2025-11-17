ee0bf85ec2689fae077a27824ca23aa4b7cb6033
Sri Kautilya Reddy Sagili <24f2005934@ds.study.iitm.ac.in>
2025-11-17 15:25:02 +0530

    Fix Playwright install for Render

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install python dependencies
 pip install -r requirements.txt
-python -m playwright install --with-deps chromium
+
+# Install Chromium dependencies manually
+apt-get update
+apt-get install -y \
+  libnss3 \
+  libatk-bridge2.0-0 \
+  libatk1.0-0 \
+  libcups2 \
+  libdrm2 \
+  libdbus-1-3 \
+  libxkbcommon0 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxfixes3 \
+  libxrandr2 \
+  libgbm1 \
+  libasound2 \
+  libatspi2.0-0 \
+  libx11-xcb1
+
+# Install Chromium browser
+python -m playwright install chromium
