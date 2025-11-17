bb02b164887f0c16607efc2fb9a6f4b7789ef976
Sri Kautilya Reddy Sagili <24f2005934@ds.study.iitm.ac.in>
2025-11-17 15:28:18 +0530

    Use browser-channel=chrome for Playwright

--- render-build.sh
+++ render-build.sh
@@ -1,27 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install python dependencies
+# Install Python packages
 pip install -r requirements.txt
 
-# Install Chromium dependencies manually
-apt-get update
-apt-get install -y \
-  libnss3 \
-  libatk-bridge2.0-0 \
-  libatk1.0-0 \
-  libcups2 \
-  libdrm2 \
-  libdbus-1-3 \
-  libxkbcommon0 \
-  libxcomposite1 \
-  libxdamage1 \
-  libxfixes3 \
-  libxrandr2 \
-  libgbm1 \
-  libasound2 \
-  libatspi2.0-0 \
-  libx11-xcb1
-
-# Install Chromium browser
-python -m playwright install chromium
+# Install Chromium in a way that works on Render (no system deps)
+python -m playwright install chromium --browser-channel=chrome
